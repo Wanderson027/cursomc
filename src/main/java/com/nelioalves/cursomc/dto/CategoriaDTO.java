b55ce46a00becfd56bed475034c81286aae4c2ef
Wandeson027 <w.rafael.s@hotmail.com>
2022-04-04 19:06:05 -0300

    adicionado filtro categoriaDTO nao finalizado

--- src/main/java/com/nelioalves/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/nelioalves/cursomc/dto/CategoriaDTO.java
@@ -3,11 +3,14 @@ package com.nelioalves.cursomc.dto;
 import java.io.Serializable;
 
 import com.nelioalves.cursomc.domain.Categoria;
+import javax.validation.constraints.NotEmpty;
 
 public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Integer id;
+	
+	@NotEmpty(message="Preenchimento obrigatorio")
 	private String nome;
 	
 	public CategoriaDTO () {
