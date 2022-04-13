78928ceb0ee3f933108fdcec10633212b18c093a
Wandeson027 <w.rafael.s@hotmail.com>
2022-04-13 16:36:46 -0300

    adicionado metodo CategoriaDTO validando tamanho nomes

--- src/main/java/com/nelioalves/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/nelioalves/cursomc/dto/CategoriaDTO.java
@@ -2,8 +2,11 @@ package com.nelioalves.cursomc.dto;
 
 import java.io.Serializable;
 
-import com.nelioalves.cursomc.domain.Categoria;
 import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.nelioalves.cursomc.domain.Categoria;
 
 public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
@@ -11,6 +14,7 @@ public class CategoriaDTO implements Serializable {
 	private Integer id;
 	
 	@NotEmpty(message="Preenchimento obrigatorio")
+	@Length(min=5, max= 80, message="O tamanho deve ser entre 5 e 80 Caracteres!")
 	private String nome;
 	
 	public CategoriaDTO () {
--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.dto.CategoriaDTO;
 import com.nelioalves.cursomc.repositories.CategoriaRepository;
 import com.nelioalves.cursomc.services.exceptions.DataIntegrityException;
 import com.nelioalves.cursomc.services.exceptions.ObjectNotFoundException;
@@ -56,7 +57,9 @@ public class CategoriaService {
 		 return repo.findAll(pageRequest);
 	 }
 	 
-	 
+	 public Categoria fromDTO(CategoriaDTO objDto) {
+		 return new Categoria(objDto.getId(), objDto.getNome());
+	 }
 	 
 	 
 }
