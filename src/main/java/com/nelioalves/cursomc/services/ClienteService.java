5a8852237a051631608be1883c07698c6bf2959c
Wandeson027 <w.rafael.s@hotmail.com>
2022-05-31 13:59:36 -0300

    Adicionado excessão ao excluir clientes com entidades

--- src/main/java/com/nelioalves/cursomc/services/ClienteService.java
+++ src/main/java/com/nelioalves/cursomc/services/ClienteService.java
@@ -39,7 +39,7 @@ public class ClienteService {
 		 try {
 			 repo.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possivel excluir uma categoria que nao possui Pordutos!");
+			throw new DataIntegrityException("Não é possivel excluir pois há entidades relacionadas!");
 		}
 		 
 	 }
